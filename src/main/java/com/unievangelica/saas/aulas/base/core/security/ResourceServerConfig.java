9c3c0b3e18b6fbfb004e2b6022b32c8e18819d3b
matheus.goncalves <matheusvieira1900@hotmail.com>
2021-03-12 18:46:54 -0300

    Primeira forma de autenticação usando BASIC. Nesse modelo os jogaores podem ser acessados sem autenticação e as demais rotas precisa passar o basic auth contendo username: admin password: 123

---
+++ src/main/java/com/unievangelica/saas/aulas/base/core/security/ResourceServerConfig.java
@@ -0,0 +1,28 @@
+package com.unievangelica.saas.aulas.base.core.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().antMatchers("/jogadores").permitAll().anyRequest().authenticated().and().httpBasic();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("123")).roles("roles");
+  }
+
+  @Bean // @Bean Facilita ao carregar em outras classes (Não precisa instanciar)
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
