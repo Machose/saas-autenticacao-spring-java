4846319c7e272341010ca18998bb6a6a4807cf90
matheus.goncalves <matheusvieira1900@hotmail.com>
2021-03-12 20:07:53 -0300

    Primeira implementação de autenticação com Bearer Token

--- src/main/java/com/unievangelica/saas/aulas/base/core/security/ResourceServerConfig.java
+++ src/main/java/com/unievangelica/saas/aulas/base/core/security/ResourceServerConfig.java
@@ -1,6 +1,8 @@
 package com.unievangelica.saas.aulas.base.core.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,12 +10,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    http.authorizeRequests().anyRequest().authenticated().and().oauth2ResourceServer().opaqueToken();
   }
 
   @Override
@@ -21,8 +24,17 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("123")).roles("roles");
   }
 
-  @Bean // @Bean Facilita ao carregar em outras classes (Não precisa instanciar)
+  // @Bean Facilita o carregamento do metodo em outras classes (Não precisa
+  // instanciar)
+  @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
+
+  @Bean
+  @Override
+  protected AuthenticationManager authenticationManager() throws Exception {
+    return super.authenticationManager();
+  }
+
 }
