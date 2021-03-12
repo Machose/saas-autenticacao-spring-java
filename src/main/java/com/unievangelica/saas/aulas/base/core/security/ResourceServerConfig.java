6cea4d7cb1e776338857d412cbb5c53a8832ec87
matheus.goncalves <matheusvieira1900@hotmail.com>
2021-03-12 18:48:34 -0300

    Removendo o acesso publico da rota de jogadores

--- src/main/java/com/unievangelica/saas/aulas/base/core/security/ResourceServerConfig.java
+++ src/main/java/com/unievangelica/saas/aulas/base/core/security/ResourceServerConfig.java
@@ -13,7 +13,7 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().antMatchers("/jogadores").permitAll().anyRequest().authenticated().and().httpBasic();
+    http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
   }
 
   @Override
